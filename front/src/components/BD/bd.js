9066e56c9cee5fd0a6f5ebc0818a5e7604cda5fd
vertelink <juandavidvertelh@gmail.com>
2023-11-03 10:07:55 -0500

    valores iniciales

---
+++ front/src/components/BD/bd.js
@@ -0,0 +1,21 @@
+export default {
+
+    hoteles: [
+        {
+            nombre: "bellavista",
+            direccion: "barichara-calle 2 # 45-43",
+            replegal: "",
+            telefono: "",
+            correo: "",
+            imagenes:[
+                {
+                    img1: "",
+                    img2: "",
+                    img3: "",
+                    img4: "",
+                }
+            ]
+        },
+     
+    ]
+}
